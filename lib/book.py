19164a3f70a69a9c495d97bbbb0c72f5b9130e3f
Clement Macharia <clementmacharia62@gmail.com>
2023-12-26 02:49:07 +0300

    added page count and title

--- lib/book.py
+++ lib/book.py
@@ -2,7 +2,15 @@
 #has the title and page_count passed into __init__, and values can be set to new instance.
 
 class Book:
+    def __init__ (self,title,page_count):
+        self.title = title
+        if (type(page_count) in (int,float)):  # checks if page_count is an integer or float.   
+            self.page_count = page_count
+            print(self.title)
+            print(self.page_count)
+        else:
+            print("page_count must be an integer")
    
-        
+men = Book("men", 23)       
     
         
