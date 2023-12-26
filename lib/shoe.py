b7810c3da8ce932214d3de3e11c68dfcf3c81656
Clement Macharia <clementmacharia62@gmail.com>
2023-12-26 03:43:28 +0300

    2

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__ (self, brand, size):
+        self.brand = brand
+
+        if (type(size) in (int,float)):
+            self.size = size
+        else:
+            print ("size must be an integer")
+
+    def cobble(self):
+        print ("says that the shoe has been repaired.")
+
+    def test_cobble_makes_new(self):
+        self.cobble()
+        self.condition = "New"
+
+
+Adidas = Shoe("Adidas", 9)
+
