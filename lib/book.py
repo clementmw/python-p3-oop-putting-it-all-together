47fab9294065ac261d0209b65cc6b6f61609e62c
Clement Macharia <clementmacharia62@gmail.com>
2023-12-26 03:17:28 +0300

    1

--- lib/book.py
+++ lib/book.py
@@ -6,11 +6,24 @@ class Book:
         self.title = title
         if (type(page_count) in (int,float)):  # checks if page_count is an integer or float.   
             self.page_count = page_count
-            print(self.title)
-            print(self.page_count)
+            self.current_page = 1
+            # print(self.title)
+            # print(self.page_count)
         else:
             print("page_count must be an integer")
+
+    def turn_page(self):
+        if self.current_page < self.page_count:
+            self.current_page += 1
+            print(f"Flipping the page...wow, you read fast!{self.current_page}")
+            return self.current_page
+            
+        else:
+            print ("the end")
+
+my_book = Book("Example Book", 100)
+my_book.turn_page()
    
-men = Book("men", 23)       
+     
     
         
